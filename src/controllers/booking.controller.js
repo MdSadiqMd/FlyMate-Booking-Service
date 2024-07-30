22da6aa85b69bc3b9d322bb5aeb23a266a4e8ee9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-30 12:00:00 +0530

    Feat: Added cancel booking Logic

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -35,23 +35,11 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
-    const idempotencyKey = req.headers["x-idempotency-key"];
-    if (!idempotencyKey) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "idempotency key missing" });
-    }
-    if (inMemDb[idempotencyKey]) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "Cannot retry on a successful payment" });
-    }
     const response = await BookingService.makePayment({
       totalCost: req.body.totalCost,
       userId: req.body.userId,
       bookingId: req.body.bookingId,
     });
-    inMemDb[idempotencyKey] = idempotencyKey;
     SuccessResponse.data = response;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -47,12 +47,19 @@ async function makePayment(data) {
       transaction
     );
     if (bookingDetails.status == CANCELLED) {
-      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+      throw new AppError(
+        "The booking has expired",
+        StatusCodes.PRECONDITION_FAILED
+      );
     }
     const bookingTime = new Date(bookingDetails.createdAt);
     const currentTime = new Date();
     if (currentTime - bookingTime > 300000) {
-      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+      await cancelBooking(data.bookingId);
+      throw new AppError(
+        "The booking has expired",
+        StatusCodes.PRECONDITION_FAILED
+      );
     }
     if (bookingDetails.totalCost != data.totalCost) {
       throw new AppError(
@@ -70,6 +77,34 @@ async function makePayment(data) {
     await bookingRepository.update(
       data.bookingId,
       { status: BOOKED },
+      transaction
+    );
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
+}
+
+async function cancelBooking(bookingId) {
+  console.log("executing cancel booking");
+  const transaction = await db.sequelize.transaction();
+  try {
+    const bookingDetails = await bookingRepository.get(bookingId, transaction);
+    if (bookingDetails.status == CANCELLED) {
+      await transaction.commit();
+      return true;
+    }
+    await axios.patch(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`,
+      {
+        seats: bookingDetails.noofSeats,
+        dec: 0,
+      }
+    );
+    await bookingRepository.update(
+      bookingId,
+      { status: CANCELLED },
       transaction
     );
     await transaction.commit();
