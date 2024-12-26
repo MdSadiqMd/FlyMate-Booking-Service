d6df506a85d108d1505b9d8ee1f81235e63c327d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-12-26 21:53:30 +0530

    configured info controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const logger = require("./config/logger.config.js");
 const Queue = require("./config/queue.config.js");
 const apiRoutes = require("./routes");
 const { CRON } = require("./utils");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -12,9 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
+app.get("/callingFlightsService", async (req, res) => {
+    const response = await axios.get('http://flymate_flights_service:3000/api/v1/info');
+    console.log(response);
+    return res.json(response.data);
+});
 
 app.listen(config.PORT, async () => {
-  logger.info(`Successfully started the server on PORT : ${config.PORT}`);
-  CRON();
-  await Queue.connectQueue();
+    logger.info(`Successfully started the server on PORT : ${config.PORT}`);
+    CRON();
+    await Queue.connectQueue();
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const bookingRoutes = require("./booking.routes");
+const { info } = require("../../controllers/info.controller");
 
 const router = express.Router();
 
 router.use("/bookings", bookingRoutes);
+router.get("/info", info);
 
 module.exports = router;
